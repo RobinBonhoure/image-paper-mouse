6de244fc9370ada9a5dff56d13aff042612e9132
robin-mk <robin.meltingk@gmail.com>
2023-07-28 11:30:21 +0200

    up

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -9,11 +9,11 @@ void main() {
     
     float dist = distance(uv,hover);
 
-    float z = hoverState * -100. * pow(sin(dist * 1. - 1.0), 3.0); 
-    newposition.z += z < 0.0 ? 0.0 : z;
+    float z = 1.0 + hoverState * -100. * pow(sin(dist * 1. - 1.), 3.); 
+    newposition.z = z < 0.0 ? 0.0 : z;
 
     vNoise = -5. * pow(sin(dist * 1. - 1.0), 3.0);
     vUv = uv;
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( newposition, 1.0 );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( newposition, 1. );
 }
